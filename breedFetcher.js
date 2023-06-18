5a806b72bac606f85699c6aa5b3a2b0265c2ffaf
Mona Lisa <anastasiia.rn@gmail.com>
2023-06-17 22:01:01 -0400

    worked on refactoring, index.js & tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,17 @@
 const request = require('request');
-const breedName = process.argv[2];
+
 
 // fetchBreedDescription: build the API URL based on the provided breedName
 // send a GET request to the API endpoint using request.get
 // handle the response in the callback
 
-const fetchBreedDescription = function(breedName) {
+//fetchBreedDescription function takes in the breedName and a callback function as parameters. Inside the function, the request logic is performed, and the callback is called with either an error or the description.
+
+//The callback is invoked with the first argument being the error (if there is one) or null if there isn't an error. The second argument of the callback is the description (if available) or null if the breed is not found.
+
+
+const fetchBreedDescription = function(breedName, callback) {
+
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
   // ?q= The q stands for "query" and is followed by the value you want to search for.
@@ -13,18 +19,17 @@ const fetchBreedDescription = function(breedName) {
 
   request.get(url, (error, response, body) => {
     if (error) {
-      console.error('Error', error);
+      callback(null, error);
       return;
     }
     const data = JSON.parse(body);
     if (data.length === 0) {
-      console.log(`Breed '${breedName}' not found.`);
+      callback(`Breed '${breedName}' not found.`, null);
     } else {
       const description = data[0].description;
-      console.log(description);
-
+      callback(null, description);
     }
   });
 };
 
-fetchBreedDescription(breedName); 
+module.exports = { fetchBreedDescription };
